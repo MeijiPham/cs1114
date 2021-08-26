705d2a104baa9290de3215eb6cc7416c4d9bac04
Meiji <meijipham0601@gmail.com>
2021-08-26 19:13:12 -0400

    Added lab1

---
+++ lab01/Level6Solution.java
@@ -0,0 +1,48 @@
+import student.micro.lightbot.*;
+
+//-------------------------------------------------------------------------
+/**
+ *  Level 6, together with its solution using a single Light-Bot.
+ *
+ * @author Meiji Pham
+ * @version 2021.08.26
+ */
+public class Level6Solution
+  extends Level6
+{
+    //~ Methods ...............................................................
+
+    // ----------------------------------------------------------
+    /**
+     * This method holds the instructions for actions the bot will take
+     * when the program runs
+     */
+    public void myProgram()
+    {
+        LightBot cal = new LightBot();
+        this.addObject(cal, 2, 6);
+        // First tile
+        cal.turnLeft();
+        cal.move();
+        cal.jump();
+        cal.jump();
+        cal.turnRight();
+        cal.jump();
+        cal.jump();
+        cal.turnLightOn();
+        // Second tile
+        cal.jump();
+        cal.turnRight();
+        for (int i = 0; i < 3; i++) {
+            cal.move();
+        }
+        cal.turnLightOn();
+        // Third tile
+        cal.turnRight();
+        cal.turnRight();
+        for (int i = 0; i < 6; i++) {
+            cal.move();
+        }
+        cal.turnLightOn();
+    }
+}
