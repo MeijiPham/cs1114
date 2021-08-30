814999c859ed650d73b90893ae2563a1e4f78766
Meiji <meijipham0601@gmail.com>
2021-08-30 11:12:08 -0400

    Added patrol man

---
+++ patrolman/PatrolBot.java
@@ -0,0 +1,16 @@
+import student.micro.lightbot.*;
+
+public class PatrolBot
+    extends LightBot
+{
+    public PatrolBot()
+    {
+        super();
+    }
+
+
+    public void patrolCastle()
+    {
+        
+    }
+}
