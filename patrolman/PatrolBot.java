8f9b665c1c8b8941111ccc29fb97caffeb21ad25
Meiji <meijipham0601@gmail.com>
2021-08-30 12:03:56 -0400

    Updated patrol man

--- patrolman/PatrolBot.java
+++ patrolman/PatrolBot.java
@@ -11,6 +11,17 @@ public class PatrolBot
 
     public void patrolCastle()
     {
-        
+        for (int i = 0; i < 4; i++) 
+        {
+            this.patrolOneWall();
+        }
+    }
+    
+    public void patrolOneWall() 
+    {
+        for (int i = 0; i < 4; i++) {
+            this.move();
+        }
+        this.turnRight();
     }
 }
